e2e0b260080ce845c50c20ea0fd4011c8af33c6e
Ali Sentas <alisentas96@gmail.com>
2017-06-18 16:18:43 +0300

    Finished repo class

--- app/src/main/java/com/alisentas/githubtrending/MainActivity.java
+++ app/src/main/java/com/alisentas/githubtrending/MainActivity.java
@@ -15,10 +15,62 @@ public class MainActivity extends AppCompatActivity {
     private class Repo {
         private String name;
         private String author;
-        
+        private String description;
+        private int stars;
+        private int starsNew;
+        private String language;
+
         // constructor
         public Repo(){
+            language = null;
+        }
 
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public int getStars() {
+            return stars;
+        }
+
+        public void setStars(int stars) {
+            this.stars = stars;
+        }
+
+        public int getStarsNew() {
+            return starsNew;
+        }
+
+        public void setStarsNew(int starsNew) {
+            this.starsNew = starsNew;
+        }
+
+        public String getLanguage() {
+            return language;
+        }
+
+        public void setLanguage(String language) {
+            this.language = language;
         }
     }
 }
